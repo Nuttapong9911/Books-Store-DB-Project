1111a6e4761e344c0c93409604eeaef66f334e47
Thanakun <Thanakun_Ch@cmu.ac.th>
2022-11-06 22:51:57 +0700

    push nav

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,8 +1,11 @@
-<header class="nav p-3 bg-dark text-white ">
+
+
+
+<header class="shadow p-3  text-white" style="background-color:rgba(0, 0, 0, 0.8);">
 
 
     <style>
-        .nav {
+        .shadow {
   box-shadow: 0 10px 20px 0 rgba(0,0,0,.2);
 }
         .right{
@@ -13,7 +16,7 @@
      padding: 10px;
         }
     </style>
-
+<div >
     <div class="container">
         <div class="d-flex justify-content-center ">
 
@@ -26,29 +29,27 @@
             <!-- Links to each pages -->
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="{{ route('home.index') }}" class="nav-link px-2 text-white font-weight-bold">Home</a></li>
-                <li><a href="#" class="nav-link px-2 text-white font-weight-bold">Features</a></li>
+                <!-- <li><a href="#" class="nav-link px-2 text-white font-weight-bold">Features</a></li>
                 <li><a href="#" class="nav-link px-2 text-white font-weight-bold">Pricing</a></li>
-                <li><a href="#" class="nav-link px-2 text-white font-weight-bold">FAQs</a></li>
+                <li><a href="#" class="nav-link px-2 text-white font-weight-bold">FAQs</a></li> -->
                 <li><a href="{{route('home.about') }}" class="nav-link px-2 text-white font-weight-bold">About</a></li>
             </ul>
 
             <!-- Search box -->
             <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" type="get" action="{{ url('/search') }}">
-            <div>
                 <div class=""><input type="search" class="form-control form-control-dark" name="query" placeholder="Search..." aria-label="Search">
-                </div>
-                <button class = "btn btn-outline-light my-2 my-sm-0 mx-3 " type="submit" >Search</button>
-            </div>    
+                </div> 
             
+
             </form>
-            
+            <button class = "btn btn-outline-light  mx-1 " type="submit" >Search</button>
             
             <!-- after login, this will show logout button -->
             @auth
             {{auth()->user()->name}}
-            <button type="button" class = "btn btn-info">
-                <a href="{{url('cart')}}" class = "btn btn-shopping-cart" >Cart</a>
-            </button>
+            <div type="button" class = "text end mx-3">
+                <a href="{{url('cart')}}" class = "btn btn-outline-light me-1" >Cart</a>
+            </div>
             <div class="text-end">
                 <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-1">Logout</a>
             </div>
@@ -65,3 +66,5 @@
         </div>
     </div>
 </header>
+
+</div>
