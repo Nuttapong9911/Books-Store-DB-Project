08d8753cdbbf0940b79a52669d527d32cb5b1174
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-11-06 12:27:15 +0700

    create controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Product;
+use App\Models\Producttype;
+use App\Models\Order;
+use App\Models\Orderdetail;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     * compact of product
+     */
+    public function index()
+    {
+        $product['products'] = Product::orderBy('product_name', 'asc')->paginate(16);
+        return view('home.index', $book);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers'], function() {
+
+    /**
+    * Home Routes
+    */
+    Route::get('/', 'ProductController@index')->name('home.index');
+
+    // Route::group(['middleware' => ['guest']], function() {
+    
+    //     /**
+    //     * Register Routes
+    //     */
+    //     Route::get('/register', 'RegisterController@show')->name('register.show');
+    //     Route::post('/register', 'RegisterController@register')->name('register.perform');
+        
+    //     /**
+    //     * Login Routes
+    //     */
+    //     Route::get('/login', 'LoginController@show')->name('login.show');
+    //     Route::post('/login', 'LoginController@login')->name('login.perform');
+
+
+
+    // });
+
+    // Route::group(['middleware' => ['auth']], function() {
+    
+    //     /**
+    //     * Logout Routes
+    //     */
+    //     Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+
+    //     Route::get('/cart', 'Bookcontroller@cart')->name('cart');
+    //     Route::get('/add-to-cart/{ISBN}', 'Bookcontroller@addToCart')->name('add.to.cart');
+    //     Route::delete('/remove-from-cart/{ISBN}', 'Bookcontroller@remove')->name('remove.from.cart');
+    // });
+
+
+
 });
