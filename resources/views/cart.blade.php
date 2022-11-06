ee5b00939fa0b1cc55bfb77b68428d57694f7804
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-11-06 21:00:00 +0700

    purchase

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -58,7 +58,7 @@
         <tr>
             <td colspan="5" class="text-right">
                 <a href="{{ url('/') }}" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a>
-                <button class="btn btn-success">Checkout</button>
+                <a href="{{ url('/purchase') }}" class="btn btn-success">Purchase</a>
             </td>
         </tr>
     </tfoot>
